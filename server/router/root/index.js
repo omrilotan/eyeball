16869ea15b0867b1d194691e023daac8c5f08538
omrilotan <omri@hamadgera.com>
2022-03-17 16:29:20 +0000

    Set root status

--- server/router/root/index.js
+++ server/router/root/index.js
@@ -3,6 +3,7 @@ import { App } from '../../../dist/views/root/index.js'
 
 export function root (req, res, next) {
   const stream = ReactDOMServer.renderToNodeStream(App())
+  es.status(403)
   res.write('<!DOCTYPE html>\n')
   stream.on('end', next)
   stream.pipe(res)
