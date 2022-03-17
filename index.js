0e8bf56c2775456ec8c9a12dcde2a787eaa69321
omrilotan <omri@hamadgera.com>
2022-03-17 16:02:16 +0000

    The last time I tried this the monkey didn't survive. Let's hope it works better this time.

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 import dotenv from 'dotenv'
 import { logger } from './server/logger/index.js'
-import pkg from './package.json'
-
-const { name, version } = pkg
 dotenv.config()
+
+const { PORT = '3000' } = process.env
 
 main()
 async function main () {
-  const { application } = await import('./server/application/index.js')
-  const app = application({ name, version })
-  app.listen(3000, () => {
-    logger.info('http://127.0.0.1:3000')
-  })
+  try {
+    const { application } = await import('./server/application/index.js')
+    const app = application()
+    app.listen(
+      PORT,
+      () => logger.info(`http://127.0.0.1:${PORT}`)
+    )
+  } catch (error) {
+    logger.error(error)
+  }
 }
